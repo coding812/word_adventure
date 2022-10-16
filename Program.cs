311535e4d39b405bc7f8ee71b1d02120ccc4a3c8
coding812 <85715642+coding812@users.noreply.github.com>
2022-10-15 23:49:26 -0400

    Update Program.cs
    added newlines and story info

--- Program.cs
+++ Program.cs
@@ -1,2 +1,12 @@
-﻿Console.WriteLine("You wake up");
-Console.WriteLine("Hello");
+﻿Console.WriteLine("You wake up and there are no lights on in the room, do you:\n" +
+    "A: Get up and turn on a light?\n" +
+    "B: Lay there and hope the lights come on themselves?\n" +
+    "C: Reach under the bed and feel around for a flashlight?\n" +
+    "\nType A,B, or C and press Enter to continue...\n");
+
+string answer_1 = Console.ReadLine();
+
+if (answer_1 == "A");
+{
+    Console.WriteLine("\nYou tripped on something...");
+}
