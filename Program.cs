1b5985d802e686927cf735055aa55d0cd65a23c3
coding812 <85715642+coding812@users.noreply.github.com>
2022-10-19 20:21:21 -0400

    New way to display story
    added a text file that the story is now read from to make code cleaner in the future

--- Program.cs
+++ Program.cs
@@ -1,8 +1,7 @@
-﻿Console.WriteLine("You wake up and there are no lights on in the room, do you:\n" +
-    "A: Get up and turn on a light?\n" +
-    "B: Lay there and hope the lights come on themselves?\n" +
-    "C: Reach under the bed and feel around for a flashlight?\n" +
-    "\nType A,B, or C and press Enter to continue...\n");
+﻿string[] lines = File.ReadAllLines(@"C:\Users\wande\source\repos\word_adventure\story_files\Story.txt");
+
+foreach (string line in lines)
+    Console.WriteLine(line);
 
 string answer_1 = Console.ReadLine();
 
